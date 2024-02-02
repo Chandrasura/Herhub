60abed59ed3655ba6e13110b898fb8aff2da8e89
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-02 06:32:55 +0100

    herhub

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div class='flex min-h-screen justify-center items-center'>
+    <h1 class="text-3xl font-bold underline text-clifford">
+        Hello world!
+      </h1>
+</div>
+@endsection
