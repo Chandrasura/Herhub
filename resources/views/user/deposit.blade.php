4bb5edcb840a6c5816822ce48809e57a7d7afd75
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-03 15:56:38 +0100

    deposit done

---
+++ resources/views/user/deposit.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+@section('content')
+    <link href="css/tabs.css" rel="stylesheet" />
+    <div class='min-h-screen w-full'>
+        <header>
+            @include('layouts.header')
+        </header>
+        <div class='h-fit w-full bg-gradient-to-r from-[#1B0037] to-[#4A0648] flex justify-center items-center'>
+            <div class="bg-white1 w-4/5 h-4/5 rounded-lg p-10 my-10 relative">
+                <div class="flex flex-col items-center justify-center p-6">
+                    <form class="w-3/5">
+                        <div class="flex justify-between items-center">
+                            <i class='bx bx-chevron-left font-bold text-2xl cursor-pointer'></i>
+                            <h3 class="text-3xl text-center uppercase font-bold text-gray-800 mb-5">Deposit</h3>
+                            <i class='text-xl'></i>
+                        </div>
+                        <div class="my-4">
+                            <div class="mb-4 flex justify-center items-center w-full">
+                                <ul class="flex rounded-lg w-4/5 flex-wrap -mb-px text-sm font-medium text-center bg-white text-frame"
+                                    id="default-tab" data-tabs-toggle="#default-tab-content" role="tablist">
+                                    <li class="w-1/2" role="presentation">
+                                        <button class="py-3 px-6 w-full rounded-lg text-[#84269C]" id="profile-tab"
+                                            data-tabs-target="#profile" type="button" role="tab"
+                                            aria-controls="profile" aria-selected="false">Deposits</button>
+                                    </li>
+                                    <li class="w-1/2" role="presentation">
+                                        <button class="py-3 px-6 w-full rounded-lg text-[#84269C]" id="dashboard-tab"
+                                            data-tabs-target="#dashboard" type="button" role="tab"
+                                            aria-controls="dashboard" aria-selected="false">History</button>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div id="default-tab-content" class="mt-8">
+                                <div class="hidden p-4 rounded-lg bg-frame" id="profile" role="tabpanel"
+                                    aria-labelledby="profile-tab">
+                                    <div class="flex flex-col p-4 text-white gap-4 justify-center items-center">
+                                        <p class="text-gray-300 text-lg">Total Balance</p>
+                                        <p class="text-xl">USDT 0.00</p>
+                                    </div>
+                                </div>
+                                <div class="hidden p-4 rounded-lg bg-frame" id="dashboard" role="tabpanel"
+                                    aria-labelledby="dashboard-tab">
+                                    <div class="flex flex-col p-4 text-white gap-4 justify-center items-center">
+                                        <p class="text-gray-300 text-lg">Total Profits</p>
+                                        <p class="text-xl">USDT 0.00</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex my-20 justify-center items-center flex-col">
+                            <button type="submit"
+                                class="py-2 w-3/5 bg-gradient-to-r from-[#28A6EF] to-[#1323A0] text-white text-md font-semibold rounded-[20px] focus:ring-4 focus:ring-blue-200 focus:outline-none focus:ring-offset-2 dark:bg-blue-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">Customer
+                                Service</button>
+                        </div>
+                    </form>
+                    <div class="absolute bottom-2">
+                        <p class="text-gray-900 font-semibold text-sm text-gray-800 dark:text-gray-300">Copyright © 2024
+                            Growthcurve. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('layouts.footer')
+    </div>
+@endsection
--- resources/views/user/info.blade.php
+++ resources/views/user/info.blade.php
@@ -2,90 +2,7 @@
 @section('content')
     <div class='min-h-screen w-full'>
         <header>
-            <nav class="bg-black border-gray-200 px-12 lg:px-6 py-4">
-                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="/" class="flex items-center">
-                        <img src="assets/logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                    </a>
-                    <div class="flex items-center lg:order-2">
-
-                        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-                            class="bg-transparent border-none outline-none focus:outline-none focus:border-none"
-                            type="button">
-                            <img src="assets/dropdown.png" class="w-5 h-5 mr-2" alt="dropdown" />
-                        </button>
-
-                        <!-- Dropdown menu -->
-                        <div id="dropdown"
-                            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                            <ul class="py-2 text-sm border border-blue-400 text-gray-700 dark:text-gray-200"
-                                aria-labelledby="dropdownDefaultButton">
-                                <li>
-                                    <a href="/profile"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b border-blue-400">Profile</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b border-blue-400">Status</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white border-b border-blue-400">Records</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">T
-                                        & C</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <button data-collapse-toggle="mobile-menu-2" type="button"
-                            class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                            aria-controls="mobile-menu-2" aria-expanded="false">
-                            <span class="sr-only">Open main menu</span>
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <a href="#"
-                                    class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                                    aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a href="#about"
-                                    class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About
-                                    Us</a>
-                            </li>
-                            <li>
-                                <a href="#certificate"
-                                    class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Certification</a>
-                            </li>
-                            <li>
-                                <a href="#customer"
-                                    class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Customer
-                                    Service</a>
-                            </li>
-                            <li>
-                                <a href="#faq"
-                                    class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">FAQ</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            @include('layouts.header')
         </header>
         <div class='h-fit w-full bg-gradient-to-r from-[#1B0037] to-[#4A0648] flex justify-center items-center'>
             <div class="bg-white w-4/5 h-4/5 rounded p-10 my-10 relative">
@@ -178,7 +95,7 @@
                             <i class="bx bx-show absolute text-xl bottom-2 right-4 cursor-pointer" id="toggle-confirm"></i>
                         </div> 
                         <div class="flex mt-8 justify-center items-center flex-col">
-                            <button type="submit" class="py-2 w-1/2 bg-gradient-to-r from-[#28A6EF] to-[#1323A0] text-white text-md font-semibold rounded-[20px] focus:ring-4 focus:ring-blue-200 focus:outline-none focus:ring-offset-2 dark:bg-blue-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">Save Changes</button>
+                            <button type="submit" class="py-2 w-3/5 bg-gradient-to-r from-[#28A6EF] to-[#1323A0] text-white text-md font-semibold rounded-[20px] focus:ring-4 focus:ring-blue-200 focus:outline-none focus:ring-offset-2 dark:bg-blue-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">Save Changes</button>
                         </div>
                     </form>
                     <div class="absolute bottom-2">
