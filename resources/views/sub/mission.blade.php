e5722de60d2df8978b7a8ed632a2f3aeb72aaae5
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-03-08 12:45:56 +0100

    welcome

---
+++ resources/views/sub/mission.blade.php
@@ -0,0 +1,29 @@
+<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative p-4 w-full max-w-2xl max-h-full">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <div class="absolute right-0 p-4 md:p-5">
+                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <div class="flex items-center md:flex-row flex-col bg-white h-fit w-full md:p-10 p-4" id="about">
+                        <div class="flex w-full md:w-1/2 justify-center items-center">
+                            <img src="assets/aero.png" class="w-3/5 h-3/5 object-contain" alt="hero" />
+                        </div>
+                        <div class="pl-6 w-full md:w-1/2 text-black">
+                            <div class="max-w-md">
+                                <h1 class="text-3xl font-normal text-black mb-5">Our Mission</h1>
+                                <p>Westmetric is a growth marketing agency focused on helping modern businesses use data to be more
+                                    creative, remove guesswork, and grow.
+                                    We are a squad of growth marketers, designers, engineers, data scientists, and mathematicians who
+                                    unlock explosive growth for tech companies and startups. We’re crazy about data and driven by the
+                                    notion of integrity.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
